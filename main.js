aa0540c87c89f11c0d1abe7887aaa5d347e486a5
Thibaut <usualksm@gmail.com>
2023-01-24 16:46:30 +0100

    P1C1_exo1 Pratiquez la création de variables

--- main.js
+++ main.js
@@ -1,40 +1,19 @@
-//*** Qu'est ce qu'une variable ?
+// Pratiquez la création de variables.
 
 /*
- Une variable est un contenant utilisé pour enregistrer une donnée spécifique dans votre programme. Une donnée placée dans une variable s'appelle une valeur.
-*/
-
-//Exemple :
-/*
-Utiliser une enveloppe pour stocker de l'argent pour des dépenses courantes, loyer. Une autre pour des économies pour une occasion particulière, voyage ou sortie.
+1. Créez une variable appelée numberOfSeasons avec la valeur 6.
+2. Créez une variable appelée numberOfEpisodes avec la valeur 12 
 */
 
 /*
-Pour savoir à quoi sert chaque enveloppe, vous devez les étiqueter : vous attribuez un nom à votre variable.
-Le nom d'une variable doit indiquer ce qui se trouver à l'intérieur, tout comme une étiquette sur un carton.
- */
-
-// Voici quelques règles générales pour la création de noms :
-
-/*
-- Utiliser des noms descriptifs dans l'ensemble de votre code.
-
-- Ne PAS utiliser des abréviations ou raccourcir des mots.
-
-- Suivre une convention de nommage constante. La convention de nommage la plus courante est 'camel case'.
-Exemple : numberOfCats, finalAnswer ect...
+Attention : Vous pourrez croiser le mot 'var' plutôt que 'let'. Pour l'instant vous pouvez simplement voir 'var' comme l'ancienne version de 'let' : c'est une autre façon de créer une variable.
 */
 
+// ECRIRE LE CODE DE L'EXERCICE CI-DESSOUS :
 
-//*** Créer une variable en la déclarant :
+let numberOfEpisodes = 6;
+let numberOfSeasons = 12;
 
-/*
-Pour utiliser une variable dans votre code, vous devez la créer, c'est ce que l'on appelle déclarer une variable.
-*/
-
-let numberOfCats = 2;
-let NumberOfDogs = 4;
-
-/*
-Ici, nous déclarons (créons) et initialisons (donnons une valeur à) deux variables : number numberOfCats et numberOfDogs.
-*/
+// code de l'exercice.
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`;
