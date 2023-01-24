1f0164da85a347e46af0b5fddad7fa40a733e71d
Thibaut <usualksm@gmail.com>
2023-01-24 16:34:05 +0100

    P1C1 première partie

---
+++ main.js
@@ -0,0 +1,40 @@
+//*** Qu'est ce qu'une variable ?
+
+/*
+ Une variable est un contenant utilisé pour enregistrer une donnée spécifique dans votre programme. Une donnée placée dans une variable s'appelle une valeur.
+*/
+
+//Exemple :
+/*
+Utiliser une enveloppe pour stocker de l'argent pour des dépenses courantes, loyer. Une autre pour des économies pour une occasion particulière, voyage ou sortie.
+*/
+
+/*
+Pour savoir à quoi sert chaque enveloppe, vous devez les étiqueter : vous attribuez un nom à votre variable.
+Le nom d'une variable doit indiquer ce qui se trouver à l'intérieur, tout comme une étiquette sur un carton.
+ */
+
+// Voici quelques règles générales pour la création de noms :
+
+/*
+- Utiliser des noms descriptifs dans l'ensemble de votre code.
+
+- Ne PAS utiliser des abréviations ou raccourcir des mots.
+
+- Suivre une convention de nommage constante. La convention de nommage la plus courante est 'camel case'.
+Exemple : numberOfCats, finalAnswer ect...
+*/
+
+
+//*** Créer une variable en la déclarant :
+
+/*
+Pour utiliser une variable dans votre code, vous devez la créer, c'est ce que l'on appelle déclarer une variable.
+*/
+
+let numberOfCats = 2;
+let NumberOfDogs = 4;
+
+/*
+Ici, nous déclarons (créons) et initialisons (donnons une valeur à) deux variables : number numberOfCats et numberOfDogs.
+*/
