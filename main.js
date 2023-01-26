0e60253d5530f04bf47476ca1943614c490d778b
Thibaut <usualksm@gmail.com>
2023-01-26 14:40:23 +0100

    P1C3_exo1 Pratiquer la création d'objet

--- main.js
+++ main.js
@@ -1,40 +1,32 @@
-/***  Définir des objets et leurs attributs avec des classes ***/
+/*** Pratiquez la création d'un objet ***/
 
 /*
-Vous avez probablement entendu auparavant le terme OBJET dans un contexte de programmation. Mais qu'est-ce que cela signifie vraiment ? 
+Dans un exercice précédent, vous avez créé trois variables pour décrire l'épisode d'une série. On dirait la situation parfaite pour créer un seul objet  episode  : créons-le maintenant !
 */
 
 /*
-Un stylo est un type d'objet : vous avez tous types de stylo : à piston, à bille, feutre, gel ect... comme faisant partie du type d'objet : STYLO.
+
+1. Créez un objet (Object, en anglais) et stockez-le dans une variable appelée ' episode' . Utilisez bien des accolades {} et mettez les trois attributs suivants :
+
+title : le titre de l'épisode ;
+
+duration : la durée de l'épisode ;
+
+hasBeenWatched : si l'épisode a été visionné ou non.
+
+Associez des valeurs appropriées à chaque attribut. N'oubliez pas d'utiliser des paires clé-valeur.
 */
 
-/*
-Cette liste mentale d'attributs sert de modèle pour cet objet. En programmation, on l'appelle une CLASSE. Pour créer une classe, vous pouvez choisir le nom de votre choix.
+// Insérer votre code ci-dessous
 
-Vous le verrez, les classes permettent aussi de regrouper beaucoup de détails, c'est pourquoi elles s'appellet aussi des TYPES CONPLEXES.
-*/
-
-/** DECOUVERTE DES OBJETS **/
-
-/*
-Les objets JS sont écrits en JSON (JavaSCript Object Notation). Ce sont des séries de paires clés-valeurs séparées par des virgules, entre des accolades. Les objets peuvent être enregistrés dans une variable :
-*/
-
-let myBook = {
-  titre: 'The Story of Tau',
-  author: 'Will Alexander',
-  numberOfPages: 250,
-  isAvailable: true
+let episode = {
+  title: 'Dark Beginning',
+  duration: 45,
+  hasBeenWatched: false
 }
 
-/*
-Chaque clé est une chaîne et les valeurs associées peuvent avoir tout type de données. (nombre, chaîne, ect..)
+// Code de l'exercice.
 
-Construire des objets présente un avantage essentiel : cela permet de regrouper les attributs d'une chose unique à un même emplacement, que ce soit un livre, un profil utilisateur ou la configuration d'une application, par exemple.
-*/
-
-/** Pratiquez la Création d'objet **/
-
-/*
-Rendez-vous la branche P1C3-exo1 pour voir l'exercice.
-*/
+document.querySelector('#episode-info').innerText = `Episode : ${episode.title}
+Duration : ${episode.duration} min
+${episode.hasBeenWatched ? 'Already Watched' : 'Not yet watched'}`;
