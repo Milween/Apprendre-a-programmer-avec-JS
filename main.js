93c85eee1d198deba10390d4298c54c7c247968e
Thibaut <usualksm@gmail.com>
2023-01-26 10:51:45 +0100

    Modifier la valeur d'une variable

--- main.js
+++ main.js
@@ -38,3 +38,56 @@ let NumberOfDogs = 4;
 /*
 Ici, nous déclarons (créons) et initialisons (donnons une valeur à) deux variables : number numberOfCats et numberOfDogs.
 */
+
+// Modifiez la valeur d'une variable.
+
+/*
+La façon la plus simple de modifier la valeur d'une variable est simplement de la réaffecter :
+*/
+let numberOfCats = 3;
+numberOfCats = 4;
+
+// Opérateurs arithmétiques - travail sur les nombres.
+
+// Pour ajouter des variables, on utilise + :
+let totalCDs = 67;
+let totalVinyls = 34;
+let totalMusic = totalCDs + totalVinyls;
+
+// Pour soutraire, on utilise - :
+let cookiesInJar = 10;
+let cookiesRemoved = 2;
+let cookiesLeftInJar = cookiesInJar - cookiesRemoved;
+
+// Pour ajouter ou soustraire un nombre d'une variable, vous pouvez utiliser les opérateurs : += et -= :
+let cookiesInJar = 10;
+/* manger deux cookies */
+cookiesInJar -= 2; // Il reste 8 cookies.
+/* cuisson d'un nouveau lot de cookies */
+cookiesInJar += 12; // Il y a maintenant 20 cookies dans la boite.
+
+// Enfin pour incrémenter ou décrémenter de 1 : on utilise ++ ou -- :
+let numbersOfLikes = 10;
+numbersOfLikes++; // Cela fait 11.
+numbersOfLikes--; // et on revient à 10.
+
+// Multiplication et division :
+
+// Les opérations de multiplication et de division utilisent les opérateurs * et / :
+let costPerProduct = 20;
+let numberOfProducts = 5;
+let totalCost = costPerProduct + numberOfProducts;
+let averageCostPerProduct = totalCost / numberOfProducts;
+
+// Comme pour l'addition et la soustraction, il exidste aussi les opérateurs *= et /= pour multiplier ou diviser un nombre.
+let numberOfCats = 2;
+numberOfCats *= 6; // vaut 2*6 = 12;
+numberOfCats /= 3; // vaut 12/3 = 4;
+
+// Pratiquez l'utilisation des opérations :
+
+/* 
+Pour vous entraîner à créer des variables, rendez-vous sur cet exercice CodePen. https://codepen.io/nicolaspatschkowski/pen/BaNxgzN
+
+*/
+
