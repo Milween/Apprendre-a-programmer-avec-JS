44d4fa437846c636bfcf6155918df54bdee4f4b4
Thibaut <usualksm@gmail.com>
2023-01-26 12:40:37 +0100

    P1C2 Enregistez vos données avec des types de données

--- main.js
+++ main.js
@@ -1,149 +1,86 @@
-//*** Qu'est ce qu'une variable ?
+/*** Enregistrez vos données avec des types de données : ***/
+
+// Une variable est composée de plusieurs éléments, découvrons les types !
+
+/** Qu'est ce qu'un type ? **/
 
 /*
- Une variable est un contenant utilisé pour enregistrer une donnée spécifique dans votre programme. Une donnée placée dans une variable s'appelle une valeur.
+Le TYPE d'une variable ou d'une constante est tout simplement le genre des données qu'elle enregistre. EN JS, il y a trois types primitifs principaux.
+
+- NUMBER
+- STRING
+- BOOLEAN
+
+Les types primitifs sont les briques de base de chaque structure de données en JS ! Il est important de comprendre leur existence et leur importance.
 */
 
-//Exemple :
+/** Découvrez le type 'NUMBER' : **/
+
 /*
-Utiliser une enveloppe pour stocker de l'argent pour des dépenses courantes, loyer. Une autre pour des économies pour une occasion particulière, voyage ou sortie.
+Toutes les variables que vous avez créez jusqu-à maitenant dans ce cours étaient de type NUMBER.
+
+En programmation, 
+
+- les nombres entiers sont aussi appelés : entiers ou integers; 
+- les nombres avec des chiffres après la virgule : virgule flottante ou floating-point.
 */
 
-/*
-Pour savoir à quoi sert chaque enveloppe, vous devez les étiqueter : vous attribuez un nom à votre variable.
-Le nom d'une variable doit indiquer ce qui se trouver à l'intérieur, tout comme une étiquette sur un carton.
- */
-
-// Voici quelques règles générales pour la création de noms :
+/* Attention à l'arithmétique en virgule flottante ! */
 
 /*
-- Utiliser des noms descriptifs dans l'ensemble de votre code.
-
-- Ne PAS utiliser des abréviations ou raccourcir des mots.
-
-- Suivre une convention de nommage constante. La convention de nommage la plus courante est 'camel case'.
-Exemple : numberOfCats, finalAnswer ect...
+L'arithmétique en virgule flottante peut déclenchez des erreurs très gênantes dans tous les langages de programmation :
 */
 
-
-//*** Créer une variable en la déclarant :
+let intergerCalculation = 1 + 2; // donne 3
+let weirdCalculation = 0.1 + 0.2; // on attend 0.3, réponse réelle 0,3000000000004
 
 /*
-Pour utiliser une variable dans votre code, vous devez la créer, c'est ce que l'on appelle déclarer une variable.
+Chaque fois que c'est possible, utilisez des calculs avec des entiers.
 */
 
-let numberOfCats = 2;
-let NumberOfDogs = 4;
+/* Appréhendez les valeurs logiques */
 
 /*
-Ici, nous déclarons (créons) et initialisons (donnons une valeur à) deux variables : number numberOfCats et numberOfDogs.
+Les valeurs logiques (booléans) sont le plus simple des types primitifs : elles peuvent avoir que deux valeurs :
+- VRAI / FAUX
+Elles s'utilisent dans toutes sortes de cas :
+- indiquer si un utilisateur est connecté ou non.
+- Si une case est cochée.
+- Si un ensemble de conditions est réuni.
 */
 
-// Modifiez la valeur d'une variable.
+let userIsSignedIn = true;
+let userIsAdmin = false;
+
+
+/** Découvrez les chaînes de caractères **/
 
 /*
-La façon la plus simple de modifier la valeur d'une variable est simplement de la réaffecter :
-*/
-let numberOfCats = 3;
-numberOfCats = 4;
+Les chaînes de caractères (STRING) sont la façon d'enregistrer du TEXTE. On peut enregistrer dans une variable de type STRING n'importe quelle chaîne de caractères.
 
-// Opérateurs arithmétiques - travail sur les nombres.
-
-// Pour ajouter des variables, on utilise + :
-let totalCDs = 67;
-let totalVinyls = 34;
-let totalMusic = totalCDs + totalVinyls;
-
-// Pour soutraire, on utilise - :
-let cookiesInJar = 10;
-let cookiesRemoved = 2;
-let cookiesLeftInJar = cookiesInJar - cookiesRemoved;
-
-// Pour ajouter ou soustraire un nombre d'une variable, vous pouvez utiliser les opérateurs : += et -= :
-let cookiesInJar = 10;
-/* manger deux cookies */
-cookiesInJar -= 2; // Il reste 8 cookies.
-/* cuisson d'un nouveau lot de cookies */
-cookiesInJar += 12; // Il y a maintenant 20 cookies dans la boite.
-
-// Enfin pour incrémenter ou décrémenter de 1 : on utilise ++ ou -- :
-let numbersOfLikes = 10;
-numbersOfLikes++; // Cela fait 11.
-numbersOfLikes--; // et on revient à 10.
-
-// Multiplication et division :
-
-// Les opérations de multiplication et de division utilisent les opérateurs * et / :
-let costPerProduct = 20;
-let numberOfProducts = 5;
-let totalCost = costPerProduct + numberOfProducts;
-let averageCostPerProduct = totalCost / numberOfProducts;
-
-// Comme pour l'addition et la soustraction, il exidste aussi les opérateurs *= et /= pour multiplier ou diviser un nombre.
-let numberOfCats = 2;
-numberOfCats *= 6; // vaut 2*6 = 12;
-numberOfCats /= 3; // vaut 12/3 = 4;
-
-// Pratiquez l'utilisation des opérations :
-
-/* 
-Pour vous entraîner à créer des variables, rendez-vous sur cet exercice CodePen. https://codepen.io/nicolaspatschkowski/pen/BaNxgzN
-
-ce rendre sur la branch P1C1_exo2 : https://github.com/Milween/Apprendre-a-programmer-avec-JS/tree/P1C1_exo2
+Ces variables sont encadrées par des guillemets simples ou doubles :
 */
 
-// Mutation des variables :
+let firstName = "Will";
+let lastName = 'Alexander';
 
 /*
-Une variable est une vase mutable, c'est-à-dire qu'elle peut changer au cours du temps. On la déclare avec le mot clé 'let' suivi d'un nom de variable et on lui affecte une valeur de départ. Par la suite, on va pouvoir changer la valeur de cette variable autant de fois que l'on souhaite.
-/*
-
-/*
-On crée un compteur à 0. A chaque fois que l'on clique sur un bouton la valeur augmente de 1.
+Les chaînes peuvent aussi être CONCATENEES par l'opérateur '+' :
 */
 
-let compteur = 0;
-compteur++;
-compteur = 10;
-
-
-// Découvrez les constantes :
+let wholeName = firstName + ' ' + lastName; // valeur: 'Will Alexander'
 
 /*
-Dans de nombreux programmes, certaines données ne seront pas modifiées pendant l'exécution du programme. C'est le cas par exemple du nom d'une entreprise, de la date de naissance d'un utilisateur, ou du nombre d'heures dans une journée. Pour s'assurer de ne pas réaffecter par inadvertance de nouvelles valeurs à ces données, vous allez utilisez des CONSTANTES.
+Il est possible depuis quelques années d'utiliser une nouvelle écriture qui simplifie la concaténation des variables : la string interpolation.
+
+Pour créer une STRING INTERPOLATION on écrit du texte encadrée par le signe ' ` ' et si on veut injecter une variable dans ce code on utilise l'expression " ${maVariable}" 
 */
 
-/*
-Ce sont simplement des variables qui ne seront pas MUTABLES. On donnera une valeur de départ et on ne pourra plus changer la valeur par la suite. Ainsi s'il y a une erreur de logique dans votre code changeant la vlaeur de la variable (constante) qui de devait pas changer, JavaScript retournera une erreur.
-*/
+const myName = 'Alexander';
+const salutation = `Bienvenue sur mon site ${myName}!`;
+console.log(salutation) // retournera 'Bienvenue sur mon site Alexander !'
 
-// par exemple:
-const nombrePostParPage = 20;
-nombrePostParPage = 30; 
-console.log(nombrePostParPage); // Retournera une erreur dans la console car ne peut plus changer sa valeur.
+/** EXERICE P1C2_exo1**/
+/* Pratiquez les types de données */
 
-// Pratiquez les constantes :
-
-/*
-C'est le moment de mettre en oeuvre ce que vous avez appris sur les opérateurs ! Rendez-vous sur ce CodePen.
-
-Voici un composant qui déconstruit une journée en heures, minutes et secondes.
-Dans l'espace alloué, créeez les trois constantes suivantes : 
-- hoursPerDay : heures par jour.
-- minutesPerHour : minutes par heure.
-- secondesPerMinutes : secondes par minute.
-
-lien vers l'exercice GitHub : 
-*/
-
-// En résumé :
-
-/*
-Dans ce chapitre, vous avez appris les bases des variables en JS, vous savez désormais :
-
-- DECLARER les variables : par le mot let 'let' et un nom en 'camelCase' et les INITIALISER '=':
-
-- MODIFIER le contenu d'une variable en la réaffectant, ou avec des opérateurs.
-
-- utiliser des CONSTANTES pour éviter le remplacement d'éléments de données essentiels.
-*/
+// Passer sur la branche P1C2_exo2
