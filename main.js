2f071374bf3032dce6436d9660bb729b46fad536
Thibaut <usualksm@gmail.com>
2023-01-26 12:48:53 +0100

    P1C2_exo1 pratiquez les types de données

--- main.js
+++ main.js
@@ -1,45 +1,26 @@
-// Pratiquez la création de variables.
+/** Pratiquez les types de données **/
 
 /*
-1. Créez une variable appelée numberOfSeasons avec la valeur 6.
-2. Créez une variable appelée numberOfEpisodes avec la valeur 12 
+Maintenant que vous avez terminé le composant pour la série, il est temps de travailler sur le composant pour les épisodes individuels.
+
+Ce composant affichera le titre de l'épisode, sa durée en minutes, et si l'utilisateur a déjà regardé l'épisode ou non.
 */
 
 /*
-Pour savoir à quoi sert chaque enveloppe, vous devez les étiqueter : vous attribuez un nom à votre variable.
-Le nom d'une variable doit indiquer ce qui se trouver à l'intérieur, tout comme une étiquette sur un carton.
- */
+1. Créez une variable nommée  episodeTitle  qui contient une chaîne de caractères correspondant au titre de votre épisode (employez votre créativité !).
 
-// Voici quelques règles générales pour la création de noms :
+2. Créez une variable appelée  episodeDuration  qui contient un nombre correspondant à la durée de votre épisode en minutes.
 
-/*
-- Utiliser des noms descriptifs dans l'ensemble de votre code.
-
-- Ne PAS utiliser des abréviations ou raccourcir des mots.
-
-- Suivre une convention de nommage constante. La convention de nommage la plus courante est 'camel case'.
-Exemple : numberOfCats, finalAnswer ect...
-*/
-
-
-//*** Créer une variable en la déclarant :
-
-/*
-Pour utiliser une variable dans votre code, vous devez la créer, c'est ce que l'on appelle déclarer une variable.
-*/
-
-let numberOfCats = 2;
-let NumberOfDogs = 4;
-
-/*
-Ici, nous déclarons (créons) et initialisons (donnons une valeur à) deux variables : number numberOfCats et numberOfDogs.
+3. Enfin, créez une variable booléenne nommée   hasBeenWatched  qui indique si l'utilisateur a déjà regardé l'épisode ou non.
 */
 
 // ECRIRE LE CODE DE L'EXERCICE CI-DESSOUS :
 
-let numberOfEpisodes = 6;
-let numberOfSeasons = 12;
+let episodeTitle = 'The first Battle';
+let episodeDuration = 45;
+let hasBeenWatched = false;
 
 // code de l'exercice.
-let paragraph = document.querySelector('#info');
-paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`;
+document.querySelector('#episode-info').innerText = `Episode : ${episodeTitle}
+Duration : ${episodeDuration} min
+${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
