f4b468647af9410c6f4523d8e439142d9e639508
Thibaut <usualksm@gmail.com>
2023-01-30 10:42:42 +0100

    P1C4_exo1 : Pratiquez la création d'un array

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-/*** Pratiquez les classes ***/
+/*** Pratiquez la création d'un array ***/
 
 /*
-La meilleure façon d'apprendre les classes est d'en créer une vous-même.
+Dans un exercice précédent, vous avez créé trois instances de la classe 'Episode' dans des variables séparées. Dans cet exercice, vous allez regrouper ces variables dans un seul tableau (array).
 
-Maintenant que vous savez créer des classes, il est temps d'en profiter pour notre composant épisode. Cette fois-ci, il y a trois composants épisodees, donc la manière logique de procéder serait de créer une classe ' Episode ', et puis en créer trois instances, pour chaque épisodes.
+1. Créer un array stocké dans une variable 'episodes', et peuplez-le avec les trois variables
+  'firstEpisode', 'secondEpisode', 'thirdEpisode'.
 
-1. Créer une classe Episode avec le mot clé ' class '.
+2. Essayez de changer l'ordre dans lequel vous avez ajoutés les épisodes, et regardez ce qu'il se passe lorsque vous rafraîchissez la page.
 
-2. Créez un constructor pour votre class qui devra accepter trois arguments :
-
-- title : le titre de l'épisode.
-- duration : la durée de l'épisode.
-- hasBeenWatched : si l'épisode a été regardé ou non (booléen)
-
-En utilisant le mot clé " this ", assignez les propriétés title, duration et hasBeenWatched via un constructor.
-
-Avec le mot clé ' new ', créez trois instances de la classe Episode :
-- firstEpisode
-- secondEpisode
-- thirdEpisode
-
-N'oubliez pas de passer des arguments appropriés à chaque instance.
 */
 
 class Episode {
@@ -31,22 +18,38 @@ class Episode {
   }
 }
 
-// Insérer votre code ci-dessous
-
 let firstEpisode = new Episode ("le mystère des égouts", 50, true)
 let secondEpisode = new Episode ("le cadavre de la Tour", 60, true)
 let thirdEpisode = new Episode ("Le musée hanté", 80, false)
 
+// Insérer votre code ci-dessous :
+
+let episodes =  [firstEpisode, secondEpisode, thirdEpisode]
+console.log(episodes[0])
+
 // Code de l'exercice.
+  //on sélectionne notre body
+const body = document.querySelector('body');
 
-document.querySelector('#first_episode-info').innerText = `Episode : ${firstEpisode.title}
-Duration : ${firstEpisode.duration} min
-${firstEpisode.hasBeenWatched ? 'Already Watched' : 'Not yet watched'}`;
+  // on crée une boucle pour parcourir touts les éléments du tableau ' episode '.
+for (let i = 0; i < 12 ; i++) {
 
-document.querySelector('#second_episode-info').innerText = `Episode : ${secondEpisode.title}
-Duration : ${secondEpisode.duration} min
-${secondEpisode.hasBeenWatched ? 'Already Watched' : 'Not yet watched'}`;
+  // On crée une div 'séries-frame'
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('series-frame');
 
-document.querySelector('#third_episode-info').innerText = `Episode : ${thirdEpisode.title}
-Duration : ${thirdEpisode.duration} min
-${thirdEpisode.hasBeenWatched ? 'Already Watched' : 'Not yet watched'}`;
+  // on crée un h2 'The Story of Tau'
+  let newTitle = document.createElement('h2');
+  newTitle.innerText = 'The Story of Tau';
+
+  // On crée un paragraphe qui prendra les valeurs de Episode pour chaque élément du tableau (épisode)
+  let newParagraph = document.createElement('p');
+  newParagraph.innerText = `${episodes[i].title}
+  ${episodes[i].duration} minutes
+  ${episodes[i].hasBeenWatched ? 'Already been Watched' : 'Not yet watched'}`;
+
+  // 'newDiv' prend comme enfant 'newTitle' et 'newParagraph' / 'newDiv' est intégré dans body.
+  newDiv.appendChild(newTitle)
+  newDiv.appendChild(newParagraph)
+  body.append(newDiv)
+}
