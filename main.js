b74103609b557a0dd43f86d942b9eb01fea59bbc
Thibaut <usualksm@gmail.com>
2023-01-31 13:55:07 +0100

    P2C2_exo2 - Mise à jour de l'énoncé

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-// CODE JAVASCRIPT DU COURS
+// PRATIQUEZ LES CONDITIONS if/else
+
+/*
+Dans cet exercice, nous allons mettre en place un contrôle de l'âge pour afficher un contenu. Sur notre page, il y a un input text qui demande de saisir votre âge, et un bouton pour valider. Si on est majeur, donc 18 ans et plus, on affiche le message autorisé, sinon on affiche une alerte disant que l’on n’est pas autorisé.
+*/
+
+/*
+Pour réaliser cet exercice, voici comment vous allez procéder :
+
+1. Vous allez lire le code JavaScript, qui est intégralement commenté, pour bien comprendre à quoi sert chaque élément, et surtout les variables à votre disposition. Il y a sûrement des éléments que vous ne comprenez pas car vous n’avez pas encore vu comment manipuler du HTML en JavaScript, mais ici vous n’avez qu’à exploiter les variables et fonctions déjà codées.
+
+2. Vous allez écrire une condition if/else entre les lignes 'INSERER VOTRE CODE' qui appellera la fonction refuser() si l'âge saisi correspond à un mineur, et la fonction valider() s’il est majeur.
+
+3. Attention, l'âge de la majorité a été stocké dans la variable ageMajorite, donc il faut utiliser cette variable dans la condition.
+
+Une fois que vous aurez codé la bonne condition, vous pourrez enchaîner les tests en saisissant différents âges et en validant.
+*/
+
+// CODE JAVASCRIPT De L'EXERCICE.
 
 // On pointe sur l'élément de message.
 const espaceMessage = document.getElementById('message')
