1fb9f68ca4e048933749aec834557f8b53ce741b
Thibaut <usualksm@gmail.com>
2023-01-31 13:49:21 +0100

    P2C2_exo1 - Pratiquez les conditions if / else

--- main.js
+++ main.js
@@ -1,73 +1,56 @@
-/*** Exécutez du JavaScript facilement ***/
+// CODE JAVASCRIPT DU COURS
 
-/** Découvrez l'avantage de JS : un langage, de nombreux environnement **/
+// On pointe sur l'élément de message.
+const espaceMessage = document.getElementById('message')
 
-/*
-L'avantage du JS, c'est que ce langage vous permet de programmer dans de nombreux environnement différents. Gardez cependant en tête que la façon d'exécuter le code est légèrement différente dans chaque environnement. Nous allons ici explorer brièvement trois environnement différents : JSBin, les pages webs et les serveurs.
-*/
+// On point suyr l'élément de bouton.
+const bouton = document.getElementById('bouton')
 
-/** Découvrez JSBIN **/
+// On pointe sur l'élément de champ de saisie.
+const ageInput = document.getElementById('age')
 
-/*
-JSBin est un excellent outil pour tester des extraits de code en JS, c'est pour cette raison que je l'ai utilisé pour démontrer certains outils ou principes. Voyons comment il exécute le code.
+// On défni la variable 'age' qu'on utilisera et une variable définissant l'âge de la majorité.
+let age;
+let ageMajorite = 18;
 
-Dans jsbin, vous pouvez écrire du code ligne par ligne dans le volet JS, et utiliser le bouton Run pour l'exécuter. JSBin parcourt alors vos lignes de code et les exéute l'une après l'autre. L'ordre est donc important.
-*/
+// fonction qui affichera le message de validation.
+function valider() {
+  espaceMessage.innerHTML = 'Vous êtes autorisé à entrer'
+}
 
-/* 
-Par exemple, ce morçeau de code :
-*/
+// fonction que affichera le message de refus.
+function refuser() {
+  alert('Cette espace est interdit aux personnes  mineurs');
+}
 
-let numberOfGuests = 20;
-console.log(numberOfGuests); //20.
+// fonction qui vérifie l'input de l'utilisation et retournera en conséquence les fonctions.
+function onConfirm() {
+  // on répère la saisie de l'âge et on transforme le texte en nombre entier.
+  age = parseInt(ageInput.value);
+  // Si la saisie n'est pas un chiffre, on affiche un message d'erreur.
+  if(isNaN(age)) {
+    alert('Ceci n\'est pas un nombre')
+    return;
+  }
 
-// n'est pas identique à :
+  // INSERER VOTRE CODE EN DESSOUS :
 
-console.log(numberOfGuests); // undefined
-let numberOfGuests = 20;
+  if (age < ageMajorite) {
+    refuser()
+  } else {
+    valider()
+  }
+  
+  // INSERER VOTRE CODE AU DESSUS :
+  ageInput.value = "";
+}
 
-/** Ecrivez du code pour des pages web **/
+// Fonction qui permet de vider l'affichage de l'élément message.
+function Reset() {
+  espaceMessage.innerHTML = '';
+}
 
-/*
-Si vous avez suivi les exercice jusqu'ici, vous avez écrit du code JS pour une page web. Mais vous n'avez pas encore vu comment votre code est interprété en arrière-plan.
-*/
+// EVENTS
 
-/*
-Eh bien, il y avait d'autres fichiers (auxquels vous n'aviez pas accès) qui effectuaient ce travail pour vous. En effet, il y avait un autre fichier JavaScript qui importait votre tableau d'épisodes et produisait les éléments qu'il ajoutait ensuite à la page web. Ce fichier était ensuite importé par le fichier HTML (qui indique au navigateur ce qu'il devrait y avoir sur la page) et exécutait automatique le code.
-*/
-
-/*
-C'est plus ou moins de cette façon que JavaScript fonctionne sur le web. Vous écrivez du code, l'enregistrez dans un ou plusieurs fichiers, puis IMPORTEZ ces fichiers par une balise spéciale dans votre fichier HTML. Le navigateur EXECUTE ensuite automatiquement le code dans ces fichiers, en général dans l'ordre de leur importation.
-*/
-
-/* Notes : Il existe un cours complet sur JS pour le web, donc quand vous autez fini celui-ci, pourquoi ne pas y jeter un oeil ?
-*/
-
-/** Ecrivez du JavaScript pour des serveurs **/
-
-/*
-Il fut un temps où JavaScript ne pouvait s'utilisait que dans des pages web (en avant plan), mais ce n'est plus vrai ! Il peut maintenant aussi s'utiliser en ARRIÈRE-PLAN, pour gérer l'accès à certaines ressources.
-*/
-
-/*
-Prenez par exemple un service de messagerie mail. Vous avez un page web qui vous permet d'accéder à vos messages, mais ils ne  sont pas ENREGISTRÉS dans cette page. Ils sont enregistrés dans une base de données à travers un serveur, qui peut être écrit en JS!
-*/
-
-/* 
-Néanmoins, les serveurs doivent être actifs à tout moment, et seuls certains éléments de code doivent être exécutés à certains moments. Par exemple, le code qui vous permet d'accéder à vos messages ne doit être exécuté QUE si et quand vous décidez d'ouvrir votre client de messagerie !
-*/
-
-/*
-Dans ce cas, un evironnement (tel que NODE); couplé à un code JS particulier, fait que cela se produit. Le serveur réagit à votre ouverture du client de messagerie, VERIFIE l'authenfication et ENVOIE vos messages.
-/*
-
-/** En RÉSUMÉ **/
-
-/* 
-Dans ce chapitre, vous avez :
-
-- appris à utiliser JSBin.
-
-- Appris à créer du JS pour les pages web.
-
-- Découvert que le JS peut aussi s'utiliser pour des serveurs.
+bouton.addEventListener('click', onConfirm)
+ageInput.addEventListener('click', Reset)
