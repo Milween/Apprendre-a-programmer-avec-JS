137d5b2f4115e7ded9640387a5cdc8d500207b24
Thibaut <usualksm@gmail.com>
2023-02-20 10:29:10 +0100

    Résumé de la partie 3

--- main.js
+++ main.js
@@ -1,40 +1,17 @@
-/*** Découverte de la résursivité. ***/
-
-/** Pratiquer la récursivité **/
+/*** Partie 3 : RÉSUMÉ ***/
 
 /*
-Quand on parle de récursivité, il y a une fonction qui fait vraiment partie des grands classiques : la fonction mathématique 'factorielle' ! 
 
-Concrètement, la factorielle d'un nombre 'n' est définie comme 'n' fois la factorielle du nombre 'n -1', et la factorielle de '1'et '1'.
-*/
+Voyons ce que vous avez appris dans cette partie du cours :
 
-/*
-Voici la décomposition de la factorielle de 3 pour mieux comprendre :
+- Vous avez étudié les FONCTIONS et leurs entrées (PARAMETRES et ARGUMENTS) ainsi que leur sorties (VALEUR DE RETOUR).
 
-factorielle(3) = 3 * factorielle(3 - 1)
+- Vous avez découvert les MÉTHODES D'INSTANCE, et la puissance supplémentaire qu'elles peuvent ajouter à vos classes.
 
-factorielle(3) = 3 * factorielle(2) =  3 * ( 2 * factorielle(2 - 1) )
+- Vous avez vu comment écrire du CODE PROPRE pour rendre vos applications plus faciles à comprendre et à maintenir.
 
-factorielle(3) = 3 * factorielle(2) =  3 * ( 2 * factorielle(1) ) = 3 * 2 * 1 = 6
+- Vous avez eu un aperçu des TESTS pour vous assurer que votre code fonctionne, et des TECHNIQUES DE DÉBOGAGE quand ce n'est pas le cas.
 
-C'est finalement une fonction simple à comprendre ccar très visuelle :
-
-factorielle(7) = 7 * 6 * 5 * 4 * 3 * 2 * 1 = 5040
-
-factorielle(4) = 4 * 3 * 2 * 1 = 24
-
-Quand on regarde la décomposition de factorielle(3), on constate qu'on appelle de nouveau la factorielle avec la valeur 'n -1', donc on est bien dans de la récursivité.
-*/
-
-/** EN RESUME **/
-
-/* Dans ce chapitre, vous avez vu :
-
-- Que les FONCTIONS RÉCURSIVES sont des fonctions qui s'appellent elles-mêmes.
-
-- Qu'une fonction récursive a besoin d'un cas de base, ou BASE CASE, pour qu'elle puisse savoir quand son travail est terminé.
-
-- Un exercice de pratique avec la fonction mathématique factorielle.
-/*
+- Enfin, vous avez exploré les FONCTIONS  RÉCURSIVES, qui s'appellent elles-mêmes pour travailler sur des structures de données arborescentes.
 
 
